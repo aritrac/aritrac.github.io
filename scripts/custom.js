66ce02103441c0211b308dbc997cfc63fbae48db
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-01 20:28:16 +0530

    New commit

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,22 +1,66 @@
 $(".hideMe").addClass("hide");
+$("#div1").toggleClass("hide");
+$("#id1").parent().toggleClass("active");
 
-$("#aboutMe").click(function(){
+$("#id1").click(function(){
   $("li").removeClass("active");
   $(this).parent().toggleClass("active");
   $(".hideMe").addClass("hide");
-  $(".aboutMe").toggleClass("hide");
+  $("#div1").toggleClass("hide");
 });
 
-$("#profile").click(function(){
+$("#id2").click(function(){
   $("li").removeClass("active");
   $(this).parent().toggleClass("active");
   $(".hideMe").addClass("hide");
-  $(".profile").toggleClass("hide");
+  $("#div2").toggleClass("hide");
 });
 
-$("#messages").click(function(){
+$("#id3").click(function(){
   $("li").removeClass("active");
   $(this).parent().toggleClass("active");
   $(".hideMe").addClass("hide");
-  $(".messages").toggleClass("hide");
+  $("#div3").toggleClass("hide");
+});
+
+$("#id4").click(function(){
+  $("li").removeClass("active");
+  $(this).parent().toggleClass("active");
+  $(".hideMe").addClass("hide");
+  $("#div4").toggleClass("hide");
+});
+
+$("#id5").click(function(){
+  $("li").removeClass("active");
+  $(this).parent().toggleClass("active");
+  $(".hideMe").addClass("hide");
+  $("#div5").toggleClass("hide");
+});
+
+$("#id6").click(function(){
+  $("li").removeClass("active");
+  $(this).parent().toggleClass("active");
+  $(".hideMe").addClass("hide");
+  $("#div6").toggleClass("hide");
+});
+
+$("#id7").click(function(){
+  $("li").removeClass("active");
+  $(this).parent().toggleClass("active");
+  $(".hideMe").addClass("hide");
+  $("#div7").toggleClass("hide");
+});
+
+$("#id8").click(function(){
+  $("li").removeClass("active");
+  $(this).parent().toggleClass("active");
+  $(".hideMe").addClass("hide");
+  $("#div8").toggleClass("hide");
+});
+
+$("#id9").click(function(){
+  $("li").removeClass("active");
+  $(this).parent().toggleClass("active");
+  $(".hideMe").addClass("hide");
+  $("#div9").toggleClass("hide");
 });
